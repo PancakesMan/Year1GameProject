f64d30cec0166b3841c1cd9e9259749cca5083e7
ProProgrammerEric <eric_work@y7mail.com>
2017-11-07 12:56:17 +1030

    Added Scene Loading Script

---
+++ Assets/Scripts/LoadSceneTrigger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneTrigger : MonoBehaviour {
+
+    public Object nextScene;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+            SceneManager.LoadScene(nextScene.name);
+    }
+}
