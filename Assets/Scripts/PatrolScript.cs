8a46e130ab414f40e447032a21a5bf035b6ecbe1
ProProgrammerEric <eric_work@y7mail.com>
2017-11-20 11:47:02 +1030

    Enemy Detects Player
    Enemy will move towards the player if it sees it. Viewing cone and distance configurable via public properties in PatrolScript

--- Assets/Scripts/PatrolScript.cs
+++ Assets/Scripts/PatrolScript.cs
@@ -8,15 +8,21 @@ public class PatrolScript : MonoBehaviour
 
     public List<GameObject> waypoints;
     public float waitTimeBetweenWaypoints = 0.0f;
+    public float viewingAngle = 120.0f;
+    public float viewingDistance = 50.0f;
+
     private float timeWaited = 0.0f;
     private int waypointTarget = 0;
+    private bool playerInSight = false;
 
     private NavMeshAgent agent;
+    private GameObject player;
 
     // Use this for initialization
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
@@ -37,6 +43,19 @@ public class PatrolScript : MonoBehaviour
 
     void NavigateTo()
     {
-        agent.SetDestination(waypoints[waypointTarget].transform.position);
+        Vector3 targetDirection = player.GetComponent<CapsuleCollider>().transform.position - transform.position;
+        float angleToPlayer = Vector3.Angle(targetDirection, transform.forward);
+
+        if (angleToPlayer < viewingAngle * 0.5f)
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, targetDirection.normalized, out hit, viewingDistance))
+                playerInSight = (hit.collider.gameObject.tag == "Player");
+        }
+
+        if (playerInSight)
+            agent.SetDestination(player.transform.position);
+        else
+            agent.SetDestination(waypoints[waypointTarget].transform.position);
     }
 }
