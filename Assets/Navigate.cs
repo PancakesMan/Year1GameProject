0a57a5e34f0d160e157d5de14ecc1a7ec2080d9c
tonymattchewson <33208378+tonymattchewson@users.noreply.github.com>
2017-11-20 12:12:13 +1030

    11/20/2017

--- Assets/Navigate.cs
+++ Assets/Navigate.cs
@@ -8,18 +8,27 @@ public class Navigate : MonoBehaviour {
     UnityEngine.AI.NavMeshAgent agent;
     Animator m_Animator;
     bool isWalking;
+    public bool updatePosition;
+    public bool updateRotation;
+    public float yRotation;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
         m_Animator = gameObject.GetComponent<Animator>();
-      //  if (isWalking == false)
-      //       m_Animator.SetBool("isWalking", false);
+        
+        //  if (isWalking == false)
+        //       m_Animator.SetBool("isWalking", false);
     }
 	
 	// Update is called once per frame
 	void Update () {
         agent.SetDestination(target.position);
-            m_Animator.SetBool("isWalking", true);
+        m_Animator.SetBool("isWalking", true);
+        if (isWalking == true) {
+            Debug.Log("Walking");
+        }
+        yRotation += Input.GetAxis("Horizontal");
+        transform.eulerAngles = new Vector3(10, yRotation, 0);
     }
 }
