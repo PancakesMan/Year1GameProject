778f0da126c1ff34d583ef63da9b2a01b0735b29
ProProgrammerEric <eric_work@y7mail.com>
2017-11-21 12:09:47 +1030

    Started on Egg Breaking Code

---
+++ Assets/Scripts/EggScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class EggScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter(Collider other)
+    {
+        if (other.tag == "Player")
+            other.GetComponent<ThirdPersonUserControl_Custom>().collidingWithEgg = true;
+    }
+
+    void OnCollisionExit(Collider other)
+    {
+        if (other.tag == "Player")
+            other.GetComponent<ThirdPersonUserControl_Custom>().collidingWithEgg = false;
+    }
+}
