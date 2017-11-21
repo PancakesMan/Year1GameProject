6739589b51a068ae12a4f683181af024ea87ad16
GrampySpoods <33010739+GrampySpoods@users.noreply.github.com>
2017-11-21 14:00:20 +1030

    Latest level 2
    Doc 1st to 3rd person switching added

---
+++ Assets/Scripts/_31Toggle.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class _31Toggle : MonoBehaviour {
+
+    public List<GameObject> ThirdPersonObjects;
+    public List<GameObject> FirstPersonObjects;
+
+    bool first, third;
+    private float cd;
+
+	// Use this for initialization
+	void Start () {
+        first = false;
+        third = true;
+        cd = 0.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        cd += Time.deltaTime;
+
+        if (Input.GetMouseButtonDown(1) && cd > 0.25f)
+        {
+            foreach (GameObject go in FirstPersonObjects)
+                go.SetActive(!first);
+
+            foreach(GameObject go in ThirdPersonObjects)
+                go.SetActive(!third);
+            
+            first = !first;
+            third = !third;
+            cd = 0.0f;
+        }
+    }
+}
