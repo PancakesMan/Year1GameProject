b85ad5bd05962a477bba18ad3438f352440213ae
PancakesMan <so_awesome12@hotmail.com>
2017-11-23 21:51:33 +1030

    Minor change to shooting script
    makes xbow play animation named "Shooting" when the right mouse button is clicked

--- Assets/Scripts/shooting.cs
+++ Assets/Scripts/shooting.cs
@@ -6,16 +6,20 @@ using UnityEngine.AI;
 public class shooting : MonoBehaviour {
 
 	public Camera mainCam;
-
 	public Animator tree;
-
     public GameObject collisionToggle;
 
-	
+    private Animator xbowAnimator;
+
+    void Start()
+    {
+        xbowAnimator = GetComponent<Animator>();
+    }
 
     void Update ()
 	{
 		if (Input.GetMouseButtonDown (0)) {
+            xbowAnimator.Play("Shooting");
 			RaycastHit hit;
 			Ray ray = mainCam.ScreenPointToRay (Input.mousePosition); 
 			if (Physics.Raycast (ray, out hit)) {
