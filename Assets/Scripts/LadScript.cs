f3eac8e94129df62ee55798b8f748c2dbdf12473
PancakesMan <so_awesome12@hotmail.com>
2017-11-26 19:10:28 +1030

    More scripts/changes

--- Assets/Scripts/LadScript.cs
+++ Assets/Scripts/LadScript.cs
@@ -6,24 +6,29 @@ using UnityEngine.AI;
 public class LadScript : MonoBehaviour {
     private NavMeshAgent agent;
     private Animator animator;
+    private Vector3 position;
+
+    public bool nearBlockade = false;
+    [HideInInspector]
+    public GameObject nearestBlockade;
 
 	// Use this for initialization
 	void Start()
     {
         agent = GetComponent<NavMeshAgent>();
         animator = GetComponent<Animator>();
+        position = transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update()
     {
-        if (animator.GetBool("eggHatched"))
+        if (nearBlockade)
         {
-            if (agent.velocity == Vector3.zero)
-                animator.Play("Lad Idle");
-            else
-                animator.Play("Walking");
+            animator.Play("Lad Eating");
+            nearestBlockade.SetActive(false);
+            nearestBlockade = null;
+            nearBlockade = false;
         }
-
 	}
 }
