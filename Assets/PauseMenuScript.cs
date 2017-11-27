cc090216c202d2e7a97c410e5163a30d3b61b10c
GrampySpoods <33010739+GrampySpoods@users.noreply.github.com>
2017-11-27 10:51:37 +1030

    Latest Level 2
    Pause menu added to ActualPrefabs folder. PauseMenuScript must be on the player.

---
+++ Assets/PauseMenuScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenuScript : MonoBehaviour
+{
+
+    public GameObject pauseMenu;
+    public bool paused = false;
+    float cd = 0.5f;
+
+    public void Update()
+    {
+        if (gameObject.name == "PlagueDoctorPrefab")
+        {
+            cd -= Time.deltaTime;
+
+            if (Input.GetKey(KeyCode.Escape) && cd < 0.0f)
+            {
+                paused = !paused;
+                cd = 0.5f;
+            }
+
+            if (paused)
+            {
+                pauseMenu.SetActive(true);
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+                Time.timeScale = 0;
+            }
+            else
+            {
+                pauseMenu.SetActive(false);
+                Cursor.lockState = CursorLockMode.Locked;
+                Cursor.visible = false;
+                Time.timeScale = 1;
+            }
+        }
+    }
+
+    public void Continue()
+    {
+        PauseMenuScript player = GameObject.Find("PlagueDoctorPrefab").GetComponent<PauseMenuScript>();
+        player.paused = false;
+    }
+}
