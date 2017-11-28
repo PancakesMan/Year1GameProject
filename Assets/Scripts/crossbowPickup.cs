4772655d0654fa357d2d3cef36b73b6433456eb8
ProProgrammerEric <eric_work@y7mail.com>
2017-11-28 10:54:33 +1030

    Play Pickup Animation

--- Assets/Scripts/crossbowPickup.cs
+++ Assets/Scripts/crossbowPickup.cs
@@ -8,15 +8,28 @@ public class crossbowPickup : MonoBehaviour {
 	public GameObject thirdTalking; 
 	public GameObject fog1;
 
+    Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
 	void OnTriggerStay (Collider other) {
 		if (other.tag == "Crossbow") {
 			if (Input.GetKeyDown (KeyCode.E))
 			{
-				crossbow.SetActive (false);
+                animator.Play("PickingUpCrossbow");
+                Invoke("DisableCrossbowObject", 2);
 				thirdTalking.SetActive (true);
 				fog1.SetActive (false); 
 				Debug.Log ("Crossbow picked up!"); 
 			}
 		}
 	}
+
+    void DisableCrossbowObject()
+    {
+        crossbow.SetActive(false);
+    }
 }
