0d1d556482f4235e16af84aeb764ae979df721b3
ProProgrammerEric <eric_work@y7mail.com>
2017-11-28 15:18:17 +1030

    suicide

---
+++ Assets/Scripts/SuicideTrigger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SuicideTrigger : MonoBehaviour {
+
+    private Animator animator;
+
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+	}
+	
+	void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Suicide")
+        {
+            animator.Play("Commit");
+        }
+    }
+}
