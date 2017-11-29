b0d131630d6783e910f0daaaf76f7030cd563245
hazelnutacorn <reinn2d@gmail.com>
2017-11-29 12:23:49 +1030

    LIt Git Cmiiotnoqwet

--- Assets/Scripts/SuicideTrigger.cs
+++ Assets/Scripts/SuicideTrigger.cs
@@ -8,6 +8,7 @@ public class SuicideTrigger : MonoBehaviour {
     private Animator animator;                           // Animator for the player
     private ThirdPersonUserControl_Custom controller;    // Controller for the player
     private _31Toggle fpsToggle;                         // Change Mode toggle script on the player
+    private bool playerTriggered = false;                // Has trigger been entered by the player?
 
     public Object scene;                                 // Next Scene to load after suiciding
     public float delay = 5.0f;                           // Delay to move to next scene after starting suicide animation
@@ -21,17 +22,32 @@ public class SuicideTrigger : MonoBehaviour {
         fpsToggle = GameObject.Find("PlagueDoctorPrefab").GetComponent<_31Toggle>();
     }
 
-    void OnDisable()
+    void Update()
     {
-        // Remove player's control
-        controller.userHasControl = false;
-        fpsToggle.active = false;
+        if (playerTriggered && Input.GetKey(KeyCode.E))
+        {
+            // Remove player's control
+            controller.userHasControl = false;
+            fpsToggle.active = false;
 
-        // Play the Commit animation
-        animator.Play("Commit");
+            // Play the Commit animation
+            animator.Play("Commit");
 
-        // Load the final scene after delay time
-        Invoke("LoadFinalScene", delay);
+            // Load the final scene after delay time
+            Invoke("LoadFinalScene", delay);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+            playerTriggered = true;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+            playerTriggered = false;
     }
 
     void LoadFinalScene()
