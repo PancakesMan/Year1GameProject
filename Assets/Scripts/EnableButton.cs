b440f3e4df1c7b0abc0f146c712a2879b5618157
ProProgrammerEric <eric_work@y7mail.com>
2017-11-29 12:37:19 +1030

    Script to enable button on delay

---
+++ Assets/Scripts/EnableButton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableButton : MonoBehaviour {
+
+    public GameObject Button;    // Button to be enabled
+    public float delay = 1.0f;   // Time before Button is enabled
+
+	// Use this for initialization
+	void Start () {
+        Invoke("Enable", delay);
+	}
+
+    void Enable()
+    {
+        Button.SetActive(true);
+    }
+}
