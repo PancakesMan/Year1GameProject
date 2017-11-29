c47eebbb1fae646465014019f821bf15553a7350
ProProgrammerEric <eric_work@y7mail.com>
2017-11-29 12:51:39 +1030

    Final Level Changes

--- Assets/Scripts/DialogueTrigger.cs
+++ Assets/Scripts/DialogueTrigger.cs
@@ -11,7 +11,7 @@ public class DialogueTrigger : MonoBehaviour {
     public bool alreadyTriggered = false;                   // Has this cutscene been triggered before?
     public bool enableModeChangeUponCompletion = true;      // Can user switch from third to first person mode after?
     public float characterTimeout;                          // Time to wait between printing each letter of the line
-    bool autoAdvance = false;                               // Automatically play the next line
+    public bool autoAdvance = false;                               // Automatically play the next line
 
     ThirdPersonUserControl_Custom charUserControl;          // Player's Controller Script
     _31Toggle modeController;                               // Player's mode toggle script
